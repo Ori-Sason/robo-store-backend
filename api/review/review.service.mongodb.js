1e4b8fcf27cafb8ebeca384cbe5f850f0a509347
Ori Sason <orisason1@gmail.com>
2022-07-18 14:12:29 +0300

    added aggregation in review query function

--- api/review/review.service.mongodb.js
+++ api/review/review.service.mongodb.js
@@ -13,15 +13,64 @@ module.exports = {
 
 async function query(filterBy) {
     try {
-        /* FIX - use filterBy to make aggregation */
+        const criteria = _buildCriteria(filterBy)
         const collection = await dbService.getCollection(COLLECTION_NAME)
-        let reviews = await collection.find({})
+
+        let reviews = await collection.aggregate([
+            {
+                $match: criteria
+            },
+            {
+                $lookup:
+                {
+                    localField: 'userId',
+                    from: 'user',
+                    foreignField: '_id',
+                    as: 'byUser'
+                }
+            },
+            {
+                $unwind: '$byUser'
+                //by default Mongo returns an array of results. 
+                //But since we are sure we will get only one user per review (since we search by ID), 
+                //unwind will convert the array to object
+            },
+            {
+                $lookup:
+                {
+                    localField: 'robotId',
+                    from: 'robot',
+                    foreignField: '_id',
+                    as: 'byRobot',
+                },
+            },
+            {
+                $unwind: '$byRobot',
+            },
+        ])
         reviews = await reviews.toArray()
+
+        reviews = reviews.map(review => {
+            review.byUser = { _id: review.byUser._id, fullname: review.byUser.fullname }
+            review.byRobot = { _id: review.byRobot._id, name: review.byRobot.name, price: review.byRobot.price, img: review.byRobot.img, owner: review.byRobot.owner }
+            review.createdAt = ObjectId(review._id).getTimestamp()
+            delete review.userId
+            delete review.robotId
+            return review
+        })
+
         return reviews
     } catch (err) {
         console.log(`ERROR: cannot find reviews (robot.service - query)`)
         throw err
     }
+}
+
+function _buildCriteria(filterBy) {
+    const criteria = {}
+    if (filterBy.byUserId) criteria.userId = ObjectId(filterBy.byUserId)
+    if (filterBy.byRobotId) criteria.robotId = ObjectId(filterBy.byRobotId)
+    return criteria
 }
 
 async function getById(reviewId) {
@@ -41,8 +90,8 @@ async function add(review, loggedInUser) {
         const collection = await dbService.getCollection(COLLECTION_NAME)
 
         const newReview = {
-            userId: loggedInUser._id,
-            robotId: review.robotId,
+            userId: ObjectId(loggedInUser._id),
+            robotId: ObjectId(review.robotId),
             title: review.title,
             rate: review.rate,
             content: review.content
@@ -65,7 +114,6 @@ async function update(review) {
         const lastModified = Date.now()
 
         const updatedReview = {
-            robotId: review.robotId,
             title: review.title,
             rate: review.rate,
             content: review.content,
